318f2ad7eaab4671555caea57d3904a10ebb5bcd
andrewrjones <andrewjones86@googlemail.com>
2011-04-07 20:01:53 +0100

    docs

--- scripts/search_dump.rb
+++ scripts/search_dump.rb
@@ -1,3 +1,8 @@
+# dumps out the result of the query, in JSON and Ruby
+# used during development
+#
+# usage: ruby scripts/search_dump.rb foo
+
 require 'rubygems'
 require 'httpclient'
 require 'json'
